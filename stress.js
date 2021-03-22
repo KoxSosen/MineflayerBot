70379e7fdfe73e387c021f1a75bb055a2e04b69c
KoxSosen <whyconfused2@gmail.com>
2021-03-22 11:16:46 +0100

    nuke unused stuff

--- stress.js
+++ stress.js
@@ -1,51 +1,28 @@
-const mineflayer = require('mineflayer');
+const mineflayer = require('mineflayer')
 const config = require("./config.json");
 
-
 if (process.argv.length < 3 || process.argv.length > 5) {
-    console.log('Usage : node stress.js <host> <port>')
-    process.exit(1)
-  }
-
-  let i = 0
-  function next () {
-    if (i < config.amount) {
-      i++
-      setTimeout(() => {
-        mineflayer.createBot(`stressbot-${i}`)
-        next()
-      }, 100)
-    }
-  }
-  next()
- 
-  const bot = (name) => {
-    mineflayer.createBot({
-      host: process.argv[2],
-      port: parseInt(process.argv[3]),
-      username: name
+  console.log('Usage : node multiple.js <host> <port>')
+  process.exit(1)
 }
-    
-    )  
 
-    bot.once('spawn', () => {
-        bot.chat(config.loginchat)
-      })
-    
-      bot.chatAddPattern(
-        /(move|move|move)/,
-        'move',
-        'move'
-      )
-    
-    const move2 =  () => {
-        bot.setControlState('forward', true)
-      }
-    
-      bot.on('move', move2)
-
-
-
+let i = 0
+function next () {
+  if (i < config.amount) {
+    i++
+    setTimeout(() => {
+      createBot(`mineflayer-bot${i}`)
+      next()
+    }, 100)
   }
+}
+next()
 
+const createBot = (name) => {
+    mineflayer.createBot({
+        host: process.argv[2],
+        port: parseInt(process.argv[3]),
+        username: name
+    });
 
+}

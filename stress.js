8f6e65c61f779a46f1ee4c5a066939a8406d23da
KoxSosen <whyconfused2@gmail.com>
2021-03-22 09:19:52 +0100

    fix issues with reference error

--- stress.js
+++ stress.js
@@ -7,6 +7,26 @@ if (process.argv.length < 3 || process.argv.length > 5) {
     process.exit(1)
   }
 
+  let i = 0
+  function next () {
+    if (i < config.amount) {
+      i++
+      setTimeout(() => {
+        createBot(`stressbot-${i}`)
+        next()
+      }, 100)
+    }
+  }
+  next()
+ 
+  const bot = function createBot (name) {
+    mineflayer.createBot({
+      host: process.argv[2],
+      port: parseInt(process.argv[3]),
+      username: name
+    })
+  }
+
 bot.once('spawn', () => {
     bot.chat(config.loginchat)
   })
@@ -23,22 +43,3 @@ const move2 =  () => {
 
   bot.on('hello', move2)
 
-  let i = 0
-function next () {
-  if (i < config.amount) {
-    i++
-    setTimeout(() => {
-      createBot(`stressbot-${i}`)
-      next()
-    }, 100)
-  }
-}
-next()
-
-function createBot (name) {
-  mineflayer.createBot({
-    host: process.argv[2],
-    port: parseInt(process.argv[3]),
-    username: name
-  })
-}

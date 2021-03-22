ffe237eeac95a5d852589df2cdc7d590df890e44
KoxSosen <whyconfused2@gmail.com>
2021-03-22 09:46:45 +0100

    func issues

--- stress.js
+++ stress.js
@@ -12,36 +12,40 @@ if (process.argv.length < 3 || process.argv.length > 5) {
     if (i < config.amount) {
       i++
       setTimeout(() => {
-        createBot(`stressbot-${i}`)
+        mineflayer.createBot(`stressbot-${i}`)
         next()
       }, 100)
     }
   }
   next()
  
-  function createBot (name) {
+  const bot = (name) => {
     mineflayer.createBot({
       host: process.argv[2],
       port: parseInt(process.argv[3]),
       username: name
-    })
+}
+    
+    )  
+
+    bot.once('spawn', () => {
+        bot.chat(config.loginchat)
+      })
+    
+      bot.chatAddPattern(
+        /(move|move|move)/,
+        'move',
+        'move'
+      )
+    
+    const move2 =  () => {
+        bot.setControlState('forward', true)
+      }
+    
+      bot.on('move', move2)
+
+
+
   }
 
 
-bot.once('spawn', () => {
-    bot.chat(config.loginchat)
-  })
-
-  bot.chatAddPattern(
-    /(move|move|move)/,
-    'move',
-    'move'
-  )
-
-const move2 =  () => {
-    bot.setControlState('forward', true)
-  }
-
-  bot.on('hello', move2)
-
-  const bot = bot
